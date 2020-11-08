692bb9e7cae5771658a77867cb52544d3bdcdaf1
Carlos Aravena <caravenaarriagada@gmail.com>
2020-11-07 23:32:39 -0300

    se añade carpeta docs

---
+++ docs/precache-manifest.c5b3ce994dd446ca8220c1fa9e8382b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e75281efe80291a98e0b42c4581e8e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "121869af06d7c3e6a59c",
+    "url": "/static/css/main.c1710e71.chunk.css"
+  },
+  {
+    "revision": "e274a80b3af2078135a2",
+    "url": "/static/js/2.b0f6abe7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b0f6abe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121869af06d7c3e6a59c",
+    "url": "/static/js/main.7c8d603d.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
